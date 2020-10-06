c8a6164bfeb2b22e7d16f4bdef19b680db448434
hexy1998 <hexy1998@outlook.com>
2020-10-06 17:47:56 -0400

    Delete compute_pi.m

--- compute_pi/compute_pi.m
+++
@@ -1,9 +0,0 @@
-function u_i = compute_pi(params, reset, r, y)
-    if reset
-        e=r-y;
-        u_i = params.k_I * params.Ts / 2 * e;
-    else
-        e=r-y;
-        u_i = params.k_I * params.Ts / 2 * (e(2) + e(1));
-    end
-end
